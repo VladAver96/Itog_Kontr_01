60812566941b4269a9ed1e8ea7f3a9e1561f4476
Vlad Aver <averyanau19gmail.com>
2024-02-01 10:45:12 +0100

    Добавил решение задачи

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+class Program
+{
+static void Main()
+{
+    Console.WriteLine("Введите элементы массива через запятую:");
+    string[] inputArray = Console.ReadLine()?.Split(',') ?? Array.Empty<string>();
+
+    string[] resultArray = FilterStrings(inputArray);
+
+    Console.WriteLine("Результат: " + string.Join(", ", resultArray));
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0;
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (!string.IsNullOrEmpty(inputArray[i]) && inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+
+        int index = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (!string.IsNullOrEmpty(inputArray[i]) && inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
